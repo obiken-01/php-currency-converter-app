7234d99646978255769ed34f76b62c43e28ca159
obiken01 <ralpharmand.alcaide@gmail.com>
2025-12-22 18:15:15 +0800

    Initial Commit

---
+++ src/components/ConverterCard.jsx
@@ -0,0 +1,8 @@
+export default function ConverterCard() {
+  return (
+    <div className="card">
+      <h1>Currency Converter</h1>
+      <p className="subtitle">Default base currency: PHP</p>
+    </div>
+  );
+}
